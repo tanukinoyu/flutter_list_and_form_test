e20d408bc5341226eab29f7ed17af7b305199ce4
tanukinoyu <tanuki.yu22@gmail.com>
2021-01-29 01:48:53 +0900

    update レイアウト調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ListPage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,6 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/home': (context) => MyHomePage(),
+        '/list': (context) => ListPage(),
+      },
       title: 'List and Form test',
       theme: ThemeData(
         primarySwatch: Colors.pink,
@@ -20,6 +26,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final titleTextController = TextEditingController();
+  final bodyTextController = TextEditingController();
+
+  String title = 'タイトル';
+  String body = '本文';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,43 +45,63 @@ class MyHomePage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: titleTextController,
                 decoration: InputDecoration(
                   hintText: 'タイトル',
                   labelText: 'Title',
                 ),
-
+                onEditingComplete: () {
+                  title = titleTextController.text;
+                },
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: bodyTextController,
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
                 decoration: InputDecoration(
                   hintText: '本文',
                   labelText: 'Body',
                 ),
-
+                onEditingComplete: () {
+                  body = bodyTextController.text;
+                },
               ),
             ),
-            Expanded(
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text('投稿する'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/list');
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.pink, width: 3),
+                  borderRadius: BorderRadius.circular(5),
+                ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       SizedBox(
                         width: 50,
                         height: 50,
                         child: Icon(Icons.wine_bar)
                       ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('title'),
-                          Text('body'),
-                        ],
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text(title),
+                            Text(body),
+                          ],
+                        ),
                       ),
                     ],
                   ),
